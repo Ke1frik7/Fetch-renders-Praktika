327b130d567928555dede395838408e7fdcee24e
Shohijahon-Musinkulovgit config --global user.name Shohijahon-Musinkulov <94446105+Shoha777@users.noreply.github.com>
2022-10-14 00:04:10 +0300

    Fech_2_end

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ fetch(link)
 .then((response) => response.json())
 .then((data) => renderCard(data))
 function renderCard (data){
-    console.log(data)
+    cards.innerHTML = null
     let arr = data.Search.slice(0, 10)
     for(let i = 0; i<arr.length; i++){
         let cloun = template.cloneNode(true)
@@ -28,4 +28,73 @@ function renderCard (data){
         cards.appendChild(cloun)
     }    
 }
+let select_cates = elementId("select_cates")
+fetch(link)
+.then((res) => res.json())
+.then((data) => options(data))
+function options(data){
+    console.log(data)
+    let array = data.Search
+    array.map((item) => {
+        let option = createTag("option")
+        option.appendChild(textNode(item.Title))
+        select_cates.appendChild(option)
+    })
+}
+let input = elementId("input")
+let objectSort = {
+    az: function(a,b){
+        if(a.Title.toLowerCase() < b.Title.toLowerCase()){
+            return -1
+        }else{
+            return 1
+        }
+    },
+    za: function(a,b){
+        if(a.Title.toLowerCase() < b.Title.toLowerCase()){
+            return 1
+        }else{
+            return -1
+        }
+    },
+    rating: function(a,b){
+        if(a.rating < b.rating){
+            return -1
+        }else{
+            return 1
+        }
+    },
+    year: function(a,b){
+        if(a.Year < b.Year){
+            return -1
+        }else{
+            return 1
+        }
+    }
+}
+let selectSorts = renderElement("#select_sorts")
+console.log(selectSorts)
+function handle(e){
+    e.preventDefault()
+    let allsArray = []
+    let value = select_cates.value
+    let rejex = new RegExp(value, "gi")
+    let inputValue = input.value
+    let rejex2 = new RegExp(inputValue, "gi")
+    let selectSort = selectSorts.value
+    fetch(link)
+    .then((res) => res.json())
+    .then((data) => {
+        let array = data.Search
+        if(value === "all"){
+            allsArray = array
+        }else if(value !== "all"){
+            allsArray = array.filter((item) => item.Title.match(rejex))
+        }
+        allsArray = array.find((item) => item.Title.match(rejex2))
+        allsArray = array.sort(objectSort[selectSort])
+        console.log(allsArray)
+    })
+}
+renderElement(".form").addEventListener("submit", handle)
 
